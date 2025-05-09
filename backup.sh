ca969906e37031d3d4984b6d11cb8bf7b670a546
Ototsky <of9494of9494@gmail.com>
2025-05-09 21:49:38 +0300

    making it rotate

--- backup.sh
+++ backup.sh
@@ -3,8 +3,30 @@
 
 location="$1"
 backup_options="-avb"
-if [ -d "$location" ]
+MAX_BACKUPS=3
+log_file="$location/backup_ids.log"
+if [ -f "$log_file" ]
 then
-    rsync "$backup_options" "$location" "$location/backup_$(date +"%Y-%m-%d_%H-%M-%S")" &> "backup_$(date +"%Y-%m-%d_%H-%M-%S").log"
-    echo "Backup created: $location/backup_$(date +"%Y-%m-%d_%H-%M-%S")"
+    last_id=$(tail -n 1 "$log_file")
+    next_id=$((last_id + 1))
+else
+    next_id=1
 fi
+
+if [ -d "$location" ]; then
+    rsync "$backup_options" --exclude="backup_" "$location" "$location/backup_$next_id" &> "$location/backup_$next_id.log"
+    echo "Backup created: $location/backup_$timestamp"
+    echo "$next_id" >> "$log_file"
+
+    if [ $(wc -l < "$log_file") -gt "$MAX_BACKUPS" ]; then
+     oldest_id=$(head -n 1 "$log_file")
+     rm -rf "$location/backup_$oldest_id"
+     sed -i "1d" "$log_file"
+    fi
+fi
+
+
+
+
+
+
